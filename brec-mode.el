9d15002c6aae50a0cef38d7113001b7c8bee29f5
Michael Allan <mike@reluk.ca>
2024-04-17 10:39:33 -0400

    Consistency, instructions in one place.

--- brec-mode.el
+++ brec-mode.el
@@ -23,23 +23,22 @@
 ;;
 ;; Installation
 ;;
-;;   If you install this package from MELPA using a package manager, then already Brec Mode should
-;;   activate for any `.brec` file you load.  Alternatively you may want to install the mode manually:
+;;   If you installed this package from MELPA using a package manager, then already Brec Mode should
+;;   activate for any `.brec` file you load.  Alternatively, you may want to install it manually:
 ;;
 ;;       1. Put a copy of the file `brec-mode.el` on your load path.
 ;;          https://www.gnu.org/software/emacs/manual/html_node/elisp/Library-Search.html
 ;;
-;;       2. Optionally compile that copy.  E.g. load it into an Emacs buffer and type
-;;          `M-x emacs-lisp-byte-compile`.
+;;       2. Optionally compile that copy.  Load it into an Emacs buffer, for example,
+;;          and type `M-x emacs-lisp-byte-compile`.
 ;;
-;;       3. Add the following code to your initialization file.
-;;          https://www.gnu.org/software/emacs/manual/html_node/emacs/Init-File.html
+;;       3. Add the following code to your Emacs initialization file.
 ;;
 ;;             (autoload 'brec-mode "brec-mode" nil t)
 ;;             (set 'auto-mode-alist (cons (cons "\\.brec\\'" 'brec-mode) auto-mode-alist))
+;;             (register-definition-prefixes "brec-mode" '("brec-"))
 ;;
-;;   For a working example of manual installation, see the relevant lines of the file
-;;   `http://reluk.ca/.config/emacs/initialization.el`.
+;;   For a working example, see the relevant lines of `http://reluk.ca/.config/emacs/initialization.el`.
 ;;
 ;; Bugs
 ;;
@@ -53,14 +52,14 @@
 ;;
 ;; Customization
 ;;
-;;   To see a list of customizeable faces, enter a Brec Mode buffer (or otherwise load Brec Mode)
+;;   To see a list of customizeable faces, enter a Brec Mode buffer, or otherwise load Brec Mode,
 ;;   and type `M-x customize-group <RET> brec <RET>`.  Alternatively, look through the `defface`
 ;;   definitions of file `brec-mode.el`.
 ;;
-;;   For reference, a working example of customization is available:
+;;   For a working example, see:
 ;;
+;;       • The author’s initialization file — http://reluk.ca/.config/emacs/initialization.el
 ;;       • The author’s `~/.Xresources` — http://reluk.ca/.Xresources
-;;       • Corresponding screen shot — http://reluk.ca/project/Breccia/Emacs/screen_shot.png
 
 ;;; Code:
 
